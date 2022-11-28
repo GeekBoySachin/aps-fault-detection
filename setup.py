40c08975d0f9ade7853c2902e252b39cdf02f8d6
GeekBoySachin <sachinv1004@gmail.com>
2022-11-28 19:23:07 +0530

    Adding gitignore,notebook,setup.py files

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+REQUIREMENT_FILE_NAME = "/config/workspace/requirements.txt"
+HYPHEN_E_DOT ="-E ."
+
+def get_requirements()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list=[requirement_file.replace("\n","") for requirement_file in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="Sachin",
+    author_email="sachinv1004@gmail.com",
+    packages=find_packages(),
+    install_requires = get_requirements()
+    )
