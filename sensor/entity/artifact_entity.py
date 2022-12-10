3e6e6120c6d55c2e7ee9d0c2f24fa4e899988f35
GeekBoySachin <sachinv1004@gmail.com>
2022-12-10 15:53:05 +0530

    Adding data transformation and model trainer

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -6,8 +6,31 @@ class DataIngestionArtifact:
     train_file_path:str 
     test_file_path:str
 
-class DataValidationArtifact:...
-class DataTransformationArtifact:...
-class ModelTrainerArtifact:...
-class ModelEvaluationArtifact:...
+@dataclass
+class DataValidationArtifact:
+    report_file_path:str
+
+
+@dataclass
+class DataTransformationArtifact:
+    transform_object_path:str
+    transformed_train_path:str
+    transformed_test_path:str
+    target_encoder_path:str
+
+@dataclass
+class ModelTrainerArtifact:
+    model_path:str 
+    f1_train_score:float 
+    f1_test_score:float
+
+@dataclass
+class ModelEvaluationArtifact:
+    is_model_accepted:bool
+    improved_accuracy:float
+    
 class ModelPusherArtifact:...
+
+
+
+
